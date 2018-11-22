c30bee90d72de853ffbf09f737d59d3ed371ad93
thomas.dacey <thomas.dacey@N30012646.hrdc-drhc.net>
2018-11-22 12:04:57 -0500

    Removed unused line of code

--- src/main/java/template/server/TemplateSeviceImpl.java
+++ src/main/java/template/server/TemplateSeviceImpl.java
@@ -7,7 +7,6 @@ import javax.jws.WebService;
 @WebService(endpointInterface = "template.server.ITemplateService")
 public class TemplateSeviceImpl implements ITemplateService {
 	public String sayHello(String myName) {
-		LocalDate date = LocalDate.of(20, 12, 20);
 		return "Hello " + myName + " !, Hope you are doing well !!";
 	}
 }
