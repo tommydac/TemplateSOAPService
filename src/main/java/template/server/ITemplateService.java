4c685ace3972b7cf36353ffc06800300afa00da8
thomas.dacey <thomas.dacey@N30012646.hrdc-drhc.net>
2018-11-22 11:57:15 -0500

    Initial proeject checkin

---
+++ src/main/java/template/server/ITemplateService.java
@@ -0,0 +1,14 @@
+package template.server;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+ 
+@WebService
+@SOAPBinding(style = Style.DOCUMENT)
+public interface ITemplateService {
+	@WebMethod()
+	String sayHello(@WebParam(name="myName") String nameThis);
+}
---
+++ src/main/java/template/server/TemplateSeviceImpl.java
@@ -0,0 +1,13 @@
+package template.server;
+
+import java.time.LocalDate;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "adjudication.server.IAService")
+public class TemplateSeviceImpl implements ITemplateService {
+	public String sayHello(String myName) {
+		LocalDate date = LocalDate.of(20, 12, 20);
+		return "Hello " + myName + " !, Hope you are doing well !!";
+	}
+}
